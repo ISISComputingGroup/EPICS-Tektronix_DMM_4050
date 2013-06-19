47f82d19cd48e6df8f50f1b6bc22f1e7590831d2
Matt Clarke <matt.clarke@stfc.ac.uk>
2013-06-19 07:31:28 +0000

    Ticket 112. A few tweaks to get it working properly. Also, included updated st.cmd files from demo.
    Former-commit-id: c1b44721409d4dbb5f14b4f4972043422eee5e6e

--- iocBoot/iocTektronix_DMM_4050/st.cmd
+++ iocBoot/iocTektronix_DMM_4050/st.cmd
@@ -15,7 +15,7 @@ Tektronix_DMM_4050_registerRecordDeviceDriver pdbbase
 lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Tektronix_DMM_4050App/protocol/Tektronix_DMM_4050.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
 
 ## Load record instances
-dbLoadRecords("db/Tektronix_DMM_4050.db","P=ex1:")
+dbLoadRecords("db/Tektronix_DMM_4050.db","P=DMM4050:")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
