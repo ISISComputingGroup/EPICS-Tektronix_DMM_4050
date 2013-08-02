fcc98ac81de3584e48559795f120c36e69092d77
Matt Clarke <matt.clarke@stfc.ac.uk>
2013-08-02 10:45:21 +0000

    Refs #172. These have been set to use devIOCStats. Also, some of the previous check-ins needed their Makefiles adjusting.
    Former-commit-id: 10bff6dcafd287a4b08d04503ed1e55306ebb9f0

--- iocBoot/iocTektronix_DMM_4050/st.cmd
+++ iocBoot/iocTektronix_DMM_4050/st.cmd
@@ -5,6 +5,9 @@
 
 < envPaths
 
+epicsEnvSet "IOCNAME" "$(P=$(MYPVPREFIX))DMM4050"
+epicsEnvSet "IOCSTATS_DB" "$(DEVIOCSTATS)/db/iocAdminSoft.db"
+
 cd ${TOP}
 
 ## Register all support components
@@ -15,7 +18,8 @@ Tektronix_DMM_4050_registerRecordDeviceDriver pdbbase
 lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Tektronix_DMM_4050App/protocol/Tektronix_DMM_4050.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
 
 ## Load record instances
-dbLoadRecords("db/Tektronix_DMM_4050.db","P=DMM4050:")
+dbLoadRecords("db/Tektronix_DMM_4050.db","P=$(IOCNAME):")
+dbLoadRecords("$(IOCSTATS_DB)","IOC=$(IOCNAME)")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
